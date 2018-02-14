4722a74ebe405fe2a245e5bf211b38f61e38ab06
Halah <halah@Ahmeds-MacBook-Pro-3.local>
2018-02-14 17:54:16 +0800

    changes root directory

---
+++ config/routes.rb
@@ -0,0 +1,17 @@
+Rails.application.routes.draw do
+  resources :passwords, controller: "clearance/passwords", only: [:create, :new]
+  resource :session, controller: "clearance/sessions", only: [:create]
+  root "welcome#index"
+  resources :users, controller: "clearance/users", only: [:create] do
+    resource :password,
+      controller: "clearance/passwords",
+      only: [:create, :edit, :update]
+  end
+
+  # get "/welcome" => "welcome#index", as: "welcome"
+  get "/sign_in" => "clearance/sessions#new", as: "sign_in"
+  delete "/sign_out" => "clearance/sessions#destroy", as: "sign_out"
+  get "/sign_up" => "clearance/users#new", as: "sign_up"
+  get "/auth/:provider/callback" => "sessions#create_from_omniauth"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
