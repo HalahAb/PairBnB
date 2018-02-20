a78746a3e67193bb43a1437a3cf3e8f6d7625d8d
Halah Abuateeq <halah.abuateeq@gmail.com>
2018-02-20 10:44:27 +0800

    added listing resource into user resources, created a controller and viewing pages

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,8 +1,31 @@
 class ListingsController < ApplicationController
+  def index
+    @listings =current_user.listings
+  end
 
   def new
   end
 
   def create
+    @listing = current_user.listings.new(listing_params)
+    if @listing.save! 
+      redirect_to 'users/#{current_user.id}/listings'
+    else
+      render 'new'
+    end
   end
+
+  def show
+    @listings = Listing.all
+  end
+
+
+  private
+
+  def listing_params
+    # {"first_name"=>"asdf", "last_name"=>"asdf"}
+    params.require(:listing).permit(:title, :description)
+  end
+
+
 end
