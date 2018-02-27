eb5bed85e3f85fd9c975226b1f21dd34425d4e81
Halah Abuateeq <halah.abuateeq@gmail.com>
2018-02-27 12:10:56 +0800

    generated a reservation job. Not needed just use active mailer for email with delideliver later and put sidkiq adapter in config file. Ignore reservation job test and rb files

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -68,6 +68,6 @@ Rails.application.configure do
 
   config.action_mailer.delivery_method = :letter_opener
   config.action_mailer.perform_deliveries = true
-  gem "letter_opener", :group => :development
+
 
 end
